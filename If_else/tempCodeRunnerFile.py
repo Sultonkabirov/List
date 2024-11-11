20296622c6ce4114283085b7ff1bc56f5e9f2e43
Abdulloh <l>
2024-11-11 17:04:02 +0500

    First commit

--- If_else/tempCodeRunnerFile.py
+++ If_else/tempCodeRunnerFile.py
@@ -1,17 +1,22 @@
-str="Man Abdulloh man mdghsjgfu"
-s=[]
-c=""
-for i in range(len(str)):
-    if str[i].isalpha():
-        c+=str[i]
-    else:
-        s.append(c)
-        c=""
-    if i==len(str)-1:
-        s.append(c)
+# lst1=[1,2,3,4,5,6,7,8,9,10,20,30,40]
+# s=[i**0.5
+#     for i in lst1 if i%2==0 if i%5==0  ]
+# print(s)
+import math
+numbers = list(range(1, 100))  # 1 dan 99 gacha bo'lgan sonlar
 
+result = (
+    [round(x**2.8 if x % 5 == 0 else x ** 3, 2) for x in numbers if x % 2 == 0 and (x**0.5 == int(x**0.5) or x % 5 != 0)]
+)[-10:]
+print(result)
+result1=[]
+for i in numbers:
+    if i%2==0 and i**0.5==int(i**0.5) or i%5!=0:
+        result1.append(round(i**2.8 if i % 5 == 0 else i ** 3, 2))
+result1.sort()
+print(result1)
+   
 
-print(s)
 
 
 
